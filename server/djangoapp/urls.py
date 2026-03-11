41c7388da9b0ea4e2d3cb581f196e3cb13fd3505
ngonied <ngoniechizororo@gmail.com>
2026-03-11 04:45:45 -0400

    reg

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
 
     path(route='logout', view=views.logout_request, name='logout'),
+    path(route = 'register', view = views.registration, name='register')
     
 
 
