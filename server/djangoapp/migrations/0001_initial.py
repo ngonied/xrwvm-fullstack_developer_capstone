3d4809db1e2e760a8564f94636fc0415435e8753
ngonied <ngoniechizororo@gmail.com>
2026-04-11 19:08:52 +0200

    linting 1

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarMake',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True, serialize=False,
+                                           verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('origin', models.CharField(max_length=50)),
@@ -25,11 +27,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CarModel',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True, serialize=False, 
+                                           verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')], default='SUV', max_length=10)),
-                ('year', models.IntegerField(default=2026, validators=[django.core.validators.MaxValueValidator(2026), django.core.validators.MinValueValidator(2016)])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='djangoapp.carmake')),
+                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')],
+                                          default='SUV', max_length=10)),
+                ('year', models.IntegerField(default=2026,
+                                             validators=[django.core.validators.MaxValueValidator(2026),
+                                                         django.core.validators.MinValueValidator(2016)])),
+                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                               to='djangoapp.carmake')),
             ],
         ),
     ]
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,11 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-
 
 class CarMake(models.Model):
     name = models.CharField(max_length = 100)
@@ -29,7 +26,6 @@ class CarModel(models.Model):
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2026,
@@ -39,27 +35,3 @@ class CarModel(models.Model):
         ])
     def __str__(self):
         return self.name
-
-
-
-
-
-
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
